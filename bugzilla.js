f04646446954df0297b8ae724f70797c5c2865c1
emceeaich <ech@emmah.net>
2018-05-07 10:10:56 -0700

    update for FFx 61, 62

--- bugzilla.js
+++ bugzilla.js
@@ -138,7 +138,7 @@ function setup_queries() {
     chfield: "[Bug creation]",
     chfieldto: "Now",
     query_format: "advanced",
-    chfieldfrom: "2016-06-01",
+    chfieldfrom: "2017-06-01",
   }, common_params);
   document.getElementById("triage-list").href = "https://bugzilla.mozilla.org/buglist.cgi?" + to_triage.toString();
   populate_table($("#need-decision"), to_triage, $("#need-decision-marker"), !!selected.length);
@@ -176,8 +176,8 @@ function setup_queries() {
     chfieldto: "Now",
     o1: "nowords",
     chfield: "[Bug creation]",
-    chfieldfrom: "2018-01-22", // change to date of first nightly of next version at release
-    f1: "cf_status_firefox60", // change to next version at release
+    chfieldfrom: "2018-03-12", // change to date of first nightly of next version at release
+    f1: "cf_status_firefox61", // change to next version at release
     resolution: "---",
     query_format: "advanced"
   }, common_params);
@@ -187,10 +187,10 @@ function setup_queries() {
 
   var stale_range = make_search({
     chfield: "[Bug creation]",
-    chfieldfrom: "2017-01-22", // change to date of first nightly of next version at release
+    chfieldfrom: "2017-03-12", // change to date of first nightly of next version at release
     chfieldto: "Now",
-    f1: "cf_status_firefox60", // increment version numbers at release 
-    f2: "cf_status_firefox61",
+    f1: "cf_status_firefox61", // increment version numbers at release 
+    f2: "cf_status_firefox62",
     j_top: "OR",
     keywords: "regression",
     keywords_type: "allwords",
