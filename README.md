c6186c4ee3456ff70b89e2451fec68db0c2bb250
Byron Jones <byron@glob.com.au>
2020-08-25 21:59:10 +0800

    Update to Python3

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # triage-center
 
-Tool for triage leads for Firefox-related components in Bugzilla to find bugs requring action. 
+Tool for triage leads for Firefox-related components in Bugzilla to find bugs
+requring action.
 
 Can be visited directly at https://mozilla.github.io/triage-center.
 
-To run locally, clone the repo, run `python process.py` in the directory and then use `python -m SimpleHTTPServer 8000` or your preferred method to bring up a static server.
+To run locally, clone the repo, run `process.py` and then use
+`python3 -m http.server` or your preferred method to bring up a static server.
 
-Uses an [SVG favicon](https://commons.wikimedia.org/wiki/File:Green_bug_and_broom.svg), provided under the GLGPL by Wikimedia User [Poznaniak](https://commons.wikimedia.org/wiki/User:Poznaniak). 
+Uses an [SVG favicon](https://commons.wikimedia.org/wiki/File:Green_bug_and_broom.svg),
+provided under the GLGPL by Wikimedia User [Poznaniak](https://commons.wikimedia.org/wiki/User:Poznaniak).
--- process.py
+++ process.py
@@ -1,24 +1,52 @@
+#!/usr/bin/env python3
 import json
-import urllib2
+import os
+import urllib.request as url_request
+from pathlib import Path
 
-fd = urllib2.urlopen("https://bugzilla.mozilla.org/rest/product?type=enterable&include_fields=id,name,is_active,components.name,components.id,components.is_active,components.description")
-d = json.load(fd)
+os.chdir(Path(__file__).resolve().parent)
+
+req = url_request.Request(
+    "https://bugzilla.mozilla.org/rest/product?%s"
+    % "&".join(
+        [
+            "type=enterable",
+            "include_fields=%s"
+            % ",".join(
+                [
+                    "id",
+                    "name",
+                    "is_active",
+                    "components.name",
+                    "components.id",
+                    "components.is_active",
+                    "components.description",
+                ]
+            ),
+        ]
+    )
+)
+with url_request.urlopen(req) as r:
+    product_list = json.load(r)
 
 components = []
-for p in d['products']:
-    if not p['is_active']:
+for product in product_list["products"]:
+    if not product["is_active"]:
         continue
-    for c in p['components']:
-        if not c['is_active']:
+    for component in product["components"]:
+        if not component["is_active"]:
             continue
-        component_data = {
-            'product_id': p['id'],
-            'product_name': p['name'],
-            'component_id': c['id'],
-            'component_name': c['name'],
-            'component_description': c['description'],
-        }
-        components.append(component_data)
+        components.append(
+            {
+                "product_id": product["id"],
+                "product_name": product["name"],
+                "component_id": component["id"],
+                "component_name": component["name"],
+                "component_description": component["description"],
+            }
+        )
 
-fd = open("components-min.json", "w")
-json.dump(components, fd)
+with open("components-min.json", "w") as f:
+    json.dump(components, f, separators=(",", ":"))
+
+print("Found %s components" % len(components))
